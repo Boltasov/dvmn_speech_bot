1df1ed9a9ca235b1ef6c6c81069b1ad8d379cd3c
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-09-10 08:52:53 +0300

    setup DialogFlow

---
+++ dialogflow_response.py
@@ -0,0 +1,44 @@
+import dotenv
+import os
+
+from google.cloud import dialogflow
+
+
+def dialogflow_response(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+    return response.query_result.fulfillment_text
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    project_id = os.getenv('PROJECT_ID')
+    session_id = 1
+    text = 'Hello'
+    language_code = 'ru-RU'
+    dialogflow_response(project_id, session_id, text, language_code)
--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import os
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 
+from dialogflow_response import dialogflow_response
+
 
 dotenv.load_dotenv()
 tg_key = os.getenv('TG_BOT_KEY')
+project_id = os.getenv('PROJECT_ID')
+language_code = 'ru-RU'
 
 updater = Updater(token=tg_key, use_context=True)
 dispatcher = updater.dispatcher
@@ -22,7 +26,10 @@ def start(update: Update, context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    input_text = update.message.text
+    session_id = update.effective_chat.id
+    response_text = dialogflow_response(project_id, session_id, input_text, language_code)
+    context.bot.send_message(chat_id=session_id, text=response_text)
 
 
 start_handler = CommandHandler('start', start)
