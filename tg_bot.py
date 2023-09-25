3c289f0832ced4ea1863e0e6da89def04a48dd2f
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-09-25 06:30:04 +0300

    more clear code

--- tg_bot.py
+++ tg_bot.py
@@ -8,16 +8,8 @@ from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandle
 from dialogflow_response import dialogflow_response
 
 
-dotenv.load_dotenv()
-tg_key = os.getenv('TG_BOT_KEY')
-project_id = os.getenv('PROJECT_ID')
-language_code = 'ru-RU'
-
-updater = Updater(token=tg_key, use_context=True)
-dispatcher = updater.dispatcher
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.WARNING)
 
 
 def start(update: Update, context: CallbackContext):
@@ -25,16 +17,25 @@ def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-def echo(update: Update, context: CallbackContext):
+def smart_response(update: Update, context: CallbackContext):
     input_text = update.message.text
     session_id = update.effective_chat.id
     response_text, _ = dialogflow_response(project_id, session_id, input_text, language_code)
     context.bot.send_message(chat_id=session_id, text=response_text)
 
 
-start_handler = CommandHandler('start', start)
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(echo_handler)
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    tg_key = os.getenv('TG_BOT_KEY')
+    project_id = os.getenv('PROJECT_ID')
+    language_code = 'ru-RU'
 
-updater.start_polling()
+    updater = Updater(token=tg_key, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    smart_handler = MessageHandler(Filters.text & (~Filters.command), smart_response)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(smart_handler)
+
+    updater.start_polling()
--- vk_bot.py
+++ vk_bot.py
@@ -1,6 +1,7 @@
 import dotenv
 import os
 import random
+import logging
 
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
@@ -8,37 +9,16 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from dialogflow_response import dialogflow_response
 
 
-dotenv.load_dotenv()
-tg_key = os.getenv('TG_BOT_KEY')
-project_id = os.getenv('PROJECT_ID')
-language_code = 'ru-RU'
-
-
-def info(vk_key):
-    vk_session = vk.VkApi(token=vk_key)
-    longpoll = VkLongPoll(vk_session)
-
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
-
-
-def echo(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
-    )
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.WARNING)
 
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
     vk_key = os.getenv('VK_KEY')
+    project_id = os.getenv('PROJECT_ID')
+    language_code = 'ru-RU'
+
     vk_session = vk.VkApi(token=vk_key)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
@@ -48,4 +28,4 @@ if __name__ == "__main__":
             input_text = event.text
             response_text, is_fallback = dialogflow_response(project_id, session_id, input_text, language_code)
             if not is_fallback:
-                vk_api.messages.send(user_id=session_id, message=response_text, random_id=0)
+                vk_api.messages.send(user_id=session_id, message=response_text, random_id=random.randint(0, 1023))
