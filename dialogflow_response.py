a62ad5cb3db88dab2a9b1d7377b69e2934fe1010
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-10-08 16:41:37 +0300

    remove code for manual testing

--- dialogflow_response.py
+++ dialogflow_response.py
@@ -22,12 +22,3 @@ def get_dialogflow_response(project_id, session_id, text, language_code):
     )
 
     return response.query_result.fulfillment_text, response.query_result.intent.is_fallback
-
-
-if __name__ == "__main__":
-    dotenv.load_dotenv()
-    project_id = os.getenv('PROJECT_ID')
-    session_id = 1
-    text = 'Hello'
-    language_code = 'ru-RU'
-    dialogflow_response(project_id, session_id, text, language_code)
